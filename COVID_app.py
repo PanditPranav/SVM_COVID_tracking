e7cba130321b4b082b2707abee62bd4e2c02fbb6
Pranav Pandit <pspandit@ucdavis.edu>
2020-08-13 21:58:49 -0700

    Update COVID_app.py

--- COVID_app.py
+++ COVID_app.py
@@ -28,10 +28,10 @@ plt.rcParams['ytick.labelsize'] = plt.rcParams['font.size']
 plt.rcParams['figure.figsize'] = 8, 8
 
 
-st.beta_set_page_config(page_title="COVID19: EpiCenter for Disease Dynamics", 
-                    page_icon="signal",
-                    layout='wide',
-                    initial_sidebar_state='auto')
+#st.beta_set_page_config(page_title="COVID19: EpiCenter for Disease Dynamics", 
+#                    page_icon="signal",
+#                    layout='wide',
+#                    initial_sidebar_state='auto')
 #@st.cache(suppress_st_warning=True)
 def plot_county(county):
     import numpy as np
