7da24b98b4c3c4cc4dc67249f7db8ed48f03746e
Pranav Pandit <pspandit@ucdavis.edu>
2024-04-10 17:10:20 -0700

    Update COVID_app.py
    Corrected the from pandas import json_normalize error

--- COVID_app.py
+++ COVID_app.py
@@ -10,7 +10,7 @@ from matplotlib.ticker import NullFormatter
 import seaborn as sns
 from urllib.request import urlopen
 import json 
-from pandas.io.json import json_normalize
+from pandas import json_normalize
 import pandas as pd
 import requests
 from matplotlib.figure import Figure
